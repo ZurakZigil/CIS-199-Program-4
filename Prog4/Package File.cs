f524f89036e2bf657dd174a2d683504ee8923469
Christopher Abbott <chris.abbott2757@gmail.com>
2020-04-17 22:34:38 -0400

    Initial commit

---
+++ Prog4/Package File.cs
@@ -0,0 +1,77 @@
+﻿/*
+Grading ID: W9957
+Program#: 4
+Due Date: 4/24/2018 11:59.00
+Course Section: 199-01
+
+Description:
+A program that utilizes classes and methods to validate package information and then presents it. This runs Program.cs.
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog4
+{
+    class Package_File
+    {
+        static void Main(string[] args)
+        {
+            //the hard coded packages we are submitting
+            GroundPackage myPackage1 = new GroundPackage(10000, 20000, 10, 10, 10, 10);
+            GroundPackage myPackage2 = new GroundPackage(78534, 95001, 25, 25, 2, 80.5);
+            GroundPackage myPackage3 = new GroundPackage(21947, 30255, 2, 2, 2, 200);
+            GroundPackage myPackage4 = new GroundPackage(11928, 80050, 125, 35, 20, 56);
+            GroundPackage myPackage5 = new GroundPackage(75923, 40602, 56.35, 35.56, 5.48, 122.25);
+
+            //a new array of packages so they can be used in the foreach loop below
+            GroundPackage[] myPackages = new GroundPackage[5];
+            myPackages[0] = myPackage1;
+            myPackages[1] = myPackage2;
+            myPackages[2] = myPackage3;
+            myPackages[3] = myPackage4;
+            myPackages[4] = myPackage5;
+
+            //changed data
+            DisplayPackages(myPackages);
+            Console.WriteLine("Changed Data\n");
+            myPackage1.OriginZip = 78912;
+            myPackage2.DestinationZip = 11082;
+            myPackage3.Width = 30;
+            myPackage4.Length = 20;
+            myPackage5.Height = 3.5;
+            DisplayPackages(myPackages);
+
+            int i = 1;
+            while(i < 10)
+            {
+                i++;
+            }
+
+            do
+            {
+                i++;
+            } while (i < 10);
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadLine();
+        }
+
+    //a method to house the loop to print each package out
+    //pre-consdition: requires all _destinationZip,_length,_width,_height,_weight
+    //post-condition: submits a string thats a list of the pre-condition
+    public static void DisplayPackages(GroundPackage[] myPackages) 
+        {
+            foreach (GroundPackage GroundPackage in myPackages)
+            {
+                Console.WriteLine("Package Information \n");
+                Console.WriteLine(GroundPackage);//list details
+                Console.WriteLine($"--------------------Total Cost: {GroundPackage.CalcCost():C2}\n");
+            }
+        }
+    }
+}
---
+++ Prog4/Program.cs
@@ -0,0 +1,176 @@
+﻿/*
+Grading ID: W9957
+Program#: 4
+Due Date: 4/24/2018 11:59.00
+Course Section: 199-01
+
+Description:
+A program that utilizes classes and methods to validate package information and then presents it
+
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog4
+{
+    public class GroundPackage
+    {
+        //The Club
+        private uint _originZip;        //original zip/shipping zip
+        private uint _destinationZip;   //destination zip/delivery zip
+        private double _length;         //the packages length
+        private double _width;          //the packages width
+        private double _height;         //the packages height
+        private double _weight;         //the packages weight
+        private uint _zoneDistance;     //the calcualted difference between zips
+
+        //The Door
+        //allows submission of values into the class
+        public GroundPackage(uint originZip, uint destinationZip, double length, double width, double height, double weight) 
+        {
+            OriginZip = originZip;
+            DestinationZip = destinationZip;
+            Length = length;
+            Width = width;
+            Height = height;
+            Weight = weight;
+        }
+
+        //validates the destination zip
+        //Has to be positive and less than 5 digits
+        //returns origin zip
+        public uint OriginZip 
+        {
+            get => _originZip;
+            set
+            {
+                if (value >= 0 && value <= 99999)
+                    _originZip = value;
+                else
+                    _originZip = 40202;
+            }
+        }
+
+        //validates the destination zip
+        //Has to be positive and less than 5 digits
+        //returns destination zip
+        public uint DestinationZip
+        {
+            get => _destinationZip;
+            set
+            {
+                if (value >= 0 && value <= 99999)
+                    _destinationZip = value;
+                else
+                    _destinationZip = 40202;
+            }
+        }
+
+        //validates the Length
+        //Has to be positive 
+        //returns double length
+        public double Length
+        {
+            get => _length;
+            set
+            {
+                if (value > 0)
+                    _length = value;
+                else
+                    _length = 1.0;
+            }
+        }
+
+        //validates the width
+        //Has to be positive 
+        //returns double width
+        public double Width
+        {
+            get => _width;
+            set
+            {
+                if (value > 0)
+                    _width = value;
+                else
+                    _width = 1.0;
+            }
+        }
+
+        //validates the Height
+        //Has to be positive 
+        //returns uint height
+        public double Height
+        {
+            get => _height;
+            set
+            {
+                if (value > 0)
+                    _height = value;
+                else
+                    _height = 1.0;
+            }
+        }
+
+        //validates the weight
+        //Has to be positive 
+        //returns uint weight
+        public double Weight
+        {
+            get => _weight;
+            set
+            {
+                if (value > 0)
+                    _weight = value;
+                else
+                    _weight = 1.0;
+            }
+        }
+
+        //creates the zone distance
+        //requires original zip and destination zip
+        //returns uint zone distance
+        public uint ZoneDistance
+        {
+            get
+            {
+                if(_originZip - _destinationZip > 0)
+                {
+                    _zoneDistance = (_originZip - _destinationZip)/ 10000;
+                    return _zoneDistance; 
+                }
+                else
+                {
+                    _zoneDistance = (_destinationZip - _originZip) / 10000;
+                    return _zoneDistance;
+                }
+            }
+        }
+
+        //all values have to be posiive and _zoneDistance required
+        //reurns double shipping cost
+        public double CalcCost()
+        {
+            double shippingCost = 0.20 * (_length + _width + _height) + 0.5 * (_zoneDistance +1)*(_weight);
+            return shippingCost;
+        }
+
+        //an overriding ToString conversion
+        //pre-consdition: requires all _destinationZip,_length,_width,_height,_weight
+        //post-condition: submits a string thats a list of the pre-condition
+        public override string ToString()
+        {
+            string newString = $"Shipping Zip: {_originZip}{Environment.NewLine}" +
+                $"Delivery Zip: {_destinationZip}{Environment.NewLine}" +
+                $"Length: { _length}{Environment.NewLine}" +
+                $"Width: { _width}{Environment.NewLine}" +
+                $"Height: { _height}{Environment.NewLine}" +
+                $"Weight: { _weight}{Environment.NewLine}";
+            return newString;
+        }
+    }
+}
